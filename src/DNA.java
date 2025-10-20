5b56f25e613f714a45d9e866e0b065d6230c17a8
zoe-704 <sunzoey@gmail.com>
2025-10-19 19:40:03 -0700

    DNA V1

--- src/DNA.java
+++ src/DNA.java
@@ -12,11 +12,32 @@
 
 public class DNA {
 
-    /**
-     * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
-     */
     public static int STRCount(String sequence, String STR) {
+        int seqLength = sequence.length(), strLength = STR.length();
+        if (strLength > seqLength) return 0; //  STR cannot be longer than the DNA sequence
 
-        return 0;
+        int maxCount = 0;
+        int i = 0;
+        while (i <= seqLength - strLength) {
+            int count = 0;
+            while (i + strLength <= seqLength && sequence.substring(i, i + strLength).equals(STR)) {
+                count++;
+                i += strLength;
+            }
+            i -= strLength;
+            maxCount= Math.max(maxCount, count);
+            i++;
+        }
+        /*
+        for (int i = 0; i <= seqLength - strLength; i++) {
+            int count = 0, j = i;
+            while (j + strLength <= seqLength && sequence.substring(j, j + strLength).equals(STR)) {
+                count++;
+                j += strLength;
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+         */
+        return maxCount;
     }
 }
