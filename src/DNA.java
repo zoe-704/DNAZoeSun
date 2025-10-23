b0ac7613bdff3379b88eed6660b40989ad2a6268
zoe-704 <sunzoey@gmail.com>
2025-10-23 15:00:13 -0700

    DNA V1

--- src/DNA.java
+++ src/DNA.java
@@ -11,11 +11,50 @@
  */
 
 public class DNA {
+    static final int R = 256;
+    static final long p = (long) 543211019;
+
+    // create a hash set
+    public static long hash(String s, int n) {
+        long h = 0;
+        for (int i = 0; i < n; i++) {
+            h = (h * R + s.charAt(i) % p);
+        }
+        return h;
+    }
 
     public static int STRCount(String sequence, String STR) {
-
         int seqLength = sequence.length(), strLength = STR.length();
         if (strLength > seqLength) return 0; //  STR cannot be longer than the DNA sequence
+
+        int maxCount = 0;
+        long strHash = hash(STR, strLength),
+                seqHash = hash(sequence.substring(0, strLength), strLength);
+
+        long[] array = new long[seqLength/strLength + 1];
+        array[0] = seqHash;
+        for (int i = strLength; i < seqLength; i++) {
+            int count = 0;
+            // begin checking for consecutive appearances
+            while (strHash == seqHash) {
+                count++;
+                // subtract first*R^(m-1) modded
+                long r_pow = 1;
+                for (int j = 0; j < i-1; j++) r_pow = r_pow*R%p;
+                long new_hash = (long) ((seqHash + p) - sequence.charAt(i - strLength) * r_pow % p) % p;
+                    seqHash = ((new_hash * R) + sequence.charAt(i)) % p;
+            }
+            maxCount = Math.max(maxCount, count);
+            if (array[i-strLength+1] == 0) {
+                array[i-strLength+1] = hash(sequence.substring(1 + (i - strLength), i+1), strLength);
+            }
+            seqHash = array[i-strLength+1];
+        }
+
+
+         /*
+         DP
+
         int dp[] = new int[seqLength], maxCount = 0;
         // iterate backwards and check if an STR sequence starts at index i
         for (int i = seqLength - strLength; i >= 0; i--) {
@@ -27,7 +66,8 @@ public class DNA {
             }
         }
 
-        /*
+
+        BASIC
         int maxCount = 0;
         int i = 0;
         while (i <= seqLength - strLength) {
