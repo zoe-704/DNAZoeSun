3f314505587a97ceebf1a310228d66de7bd47bab
zoe-704 <sunzoey@gmail.com>
2025-10-27 22:04:44 -0700

    Final version

--- src/DNA.java
+++ src/DNA.java
@@ -26,65 +26,39 @@ public class DNA {
     public static int STRCount(String sequence, String STR) {
         int seqLength = sequence.length(), strLength = STR.length();
         if (strLength > seqLength) return 0; //  STR cannot be longer than the DNA sequence
+        // Initialize counters and index to 0
+        int maxCount = 0, count = 0, i = 0;
 
-        int maxCount = 0, count = 0;
-        // hash for STR and first part of sequence
-        long strHash = hash(STR, 0, strLength), seqHash = hash(sequence, 0, strLength);
+        // Hash STR and first subsequence of length strLength
+        long strHash = hash(STR, 0, strLength),
+                seqHash = hash(sequence, 0, strLength);
 
         // Iterate through the entire sequence
-        for (int i = 0; i <= seqLength - strLength; i++) {
-            // Increment counter if subsequence and STR hashes match
+        while (i < seqLength) {
+            // Match: increment counter and go to next consecutive candidate
             if (strHash == seqHash) {
                 count++;
-                maxCount = Math.max(maxCount, count);
-                i += strLength-1; // move to next candidate
-                if (i + strLength < seqLength) {
-                    seqHash = hash(sequence, i + 1, strLength);
-                }
-            } else { // Subsequence and STR hashes do not match
-                // Remove first letter and add letter to subsequence hash
-                count = 0;
-                long first = (long)Math.pow(RADIX, strLength - 1);
-                if (i + strLength < seqLength) {
-                    seqHash = ((seqHash - sequence.charAt(i) * first % P + P) % P);
-                    seqHash = (seqHash * RADIX + sequence.charAt(i + strLength)) % P;
-                }
-
-                //seqHash = ((seqHash - sequence.charAt(i) * first % P + P) % P);
-                //seqHash = (seqHash * RADIX + sequence.charAt(i + strLength)) % P;
-
-                //seqHash = (seqHash + P) - (sequence.charAt(i) * first % P) % P;
-                //seqHash = (seqHash * RADIX + sequence.charAt(i + strLength)) % P;
+                i += strLength;
             }
+            // No match: update counters and increment index by 1
+            else {
+                maxCount = Math.max(count, maxCount);
+                count = 0;
+                i++;
+            }
+            // Rehash for current subsequence
+            if (i + strLength < seqLength) {
+                seqHash = hash(sequence, i, strLength);
+            }
+            maxCount = Math.max(maxCount, count); // Update max
         }
         return maxCount;
     }
 }
 
-    /*
-            long curHash = hash(sequence.substring(j, j + strLength), strLength);;
-            // begin checking for consecutive appearances
-            while (j + strLength <= seqLength && curHash == strHash) {
-                count++;
-                j += strLength;
-                // Update hash for next substring window
-                if (j + strLength <= seqLength) {
-                    curHash = (curHash + P - sequence.charAt(j - strLength) * RM % P) % P;
-                    curHash = (curHash * RADIX + sequence.charAt(j + strLength - 1)) % P;
-                }
-            }
-            maxCount = Math.max(maxCount, count);
-
-            // Move rolling window by 1 character for outer loop
-            if (i + strLength < seqLength) {
-                seqHash = (seqHash + P - sequence.charAt(i) * RM % P) % P;
-                seqHash = (seqHash * RADIX + sequence.charAt(i + strLength)) % P;
-            }
-
-        }
         /* DP approach
         int dp[] = new int[seqLength], maxCount = 0;
-        // iterate backwards and check if an STR sequence starts at index i
+        // Iterate backwards and check if an STR sequence starts at index i
         for (int i = seqLength - strLength; i >= 0; i--) {
             if (sequence.substring(i, i + strLength).equals(STR)) {
                 if (i + strLength < seqLength) {
@@ -93,8 +67,6 @@ public class DNA {
                 maxCount = Math.max(maxCount, dp[i]);
             }
         }
-        */
-    /*
         return maxCount;
 
     }
